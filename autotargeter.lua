d49f1a257c32299774aea245c6942e9a2c6c94d0
Suprcheese <Suprcheese@users.noreply.github.com>
2016-06-03 19:20:03 -0700

    Randomize target selection

--- autotargeter.lua
+++ autotargeter.lua
@@ -4,11 +4,11 @@ function findNestNear(entity)
 	local search = Position.expand_to_area(entity.position, autoTargetRange)
 	local spawners = entity.surface.find_entities_filtered{area = search, type = "unit-spawner"}
 	if #spawners > 0 then
-		return spawners[1]
+		return spawners[math.random(#spawners)]
 	end
 	local worms = entity.surface.find_entities_filtered{area = search, type = "turret"}
 	if #worms > 0 then
-		return worms[1]
+		return worms[math.random(#worms)]
 	end
 	return false
 end
