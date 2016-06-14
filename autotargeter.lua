67d1120a188dfb19302d0b6280309b6b566aff55
Suprcheese <Suprcheese@users.noreply.github.com>
2016-06-14 10:51:30 -0700

    Only print message if an ion cannon is actually available for auto-targeting

--- autotargeter.lua
+++ autotargeter.lua
@@ -18,10 +18,10 @@ script.on_event(defines.events.on_sector_scanned, function(event)
 	if radar.name == "auto-targeter" then
 		local target = findNestNear(radar)
 		if target then
-			if printMessages then
+			local fired = targetIonCannon(radar.force, target.position, radar.surface)
+			if fired and printMessages then
 				Game.print_force(radar.force, {"auto-target-designated", radar.backer_name, target.position.x, target.position.y})
 			end
-			targetIonCannon(radar.force, target.position, radar.surface)
 		end
 	end
 end)
