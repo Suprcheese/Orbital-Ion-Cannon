6cf65fa6a3b45b1b7e338d4d5dfb997a9ee8e3f1
Suprcheese <Suprcheese@users.noreply.github.com>
2018-01-22 14:31:55 -0800

    Was-too-hasty, spaces come back

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -1,7 +1,7 @@
 local ion_cannon_targeter = util.table.deepcopy(data.raw["ammo-turret"]["gun-turret"])
 
 ion_cannon_targeter.name = "ion-cannon-targeter"
-ion_cannon_targeter.icon = "__Orbital-Ion-Cannon__/graphics/crosshairs64.png"
+ion_cannon_targeter.icon = "__Orbital Ion Cannon__/graphics/crosshairs64.png"
 ion_cannon_targeter.icon_size = 64
 ion_cannon_targeter.flags = {"placeable-neutral", "player-creation", "placeable-off-grid"}
 ion_cannon_targeter.collision_mask = {}
@@ -14,7 +14,7 @@ ion_cannon_targeter.folded_animation =
 	layers =
 	{
 		{
-			filename = "__Orbital-Ion-Cannon__/graphics/null.png",
+			filename = "__Orbital Ion Cannon__/graphics/null.png",
 			priority = "medium",
 			width = 32,
 			height = 32,
@@ -32,7 +32,7 @@ ion_cannon_targeter.base_picture =
 	layers =
 	{
 		{
-			filename = "__Orbital-Ion-Cannon__/graphics/crosshairs64.png",
+			filename = "__Orbital Ion Cannon__/graphics/crosshairs64.png",
 			line_length = 1,
 			width = 64,
 			height = 64,
@@ -182,7 +182,7 @@ data:extend({
 		light = {intensity = 0, size = 0},
 		animation =
 		{
-			filename = "__Orbital-Ion-Cannon__/graphics/null.png",
+			filename = "__Orbital Ion Cannon__/graphics/null.png",
 			priority = "low",
 			width = 32,
 			height = 32,
@@ -190,7 +190,7 @@ data:extend({
 		},
 		shadow =
 		{
-			filename = "__Orbital-Ion-Cannon__/graphics/null.png",
+			filename = "__Orbital Ion Cannon__/graphics/null.png",
 			priority = "low",
 			width = 32,
 			height = 32,
@@ -243,7 +243,7 @@ data:extend({
 		light = {intensity = 0, size = 0},
 		animation =
 		{
-			filename = "__Orbital-Ion-Cannon__/graphics/null.png",
+			filename = "__Orbital Ion Cannon__/graphics/null.png",
 			priority = "low",
 			width = 32,
 			height = 32,
@@ -251,7 +251,7 @@ data:extend({
 		},
 		shadow =
 		{
-			filename = "__Orbital-Ion-Cannon__/graphics/null.png",
+			filename = "__Orbital Ion Cannon__/graphics/null.png",
 			priority = "low",
 			width = 32,
 			height = 32,
@@ -265,7 +265,7 @@ data:extend({
 		variations =
 		{
 			{
-				filename = "__Orbital-Ion-Cannon__/sound/Klaxon.ogg",
+				filename = "__Orbital Ion Cannon__/sound/Klaxon.ogg",
 				volume = 0.75
 			},
 		},
@@ -284,7 +284,7 @@ data:extend({
 		animations =
 		{
 			{
-				filename = "__Orbital-Ion-Cannon__/graphics/crosshairs_anim.png",
+				filename = "__Orbital Ion Cannon__/graphics/crosshairs_anim.png",
 				priority = "low",
 				width = 64,
 				height = 64,
@@ -298,7 +298,7 @@ data:extend({
 		-- pictures =
 		-- {
 			-- {
-				-- filename = "__Orbital-Ion-Cannon__/graphics/crosshairsEntity.png",
+				-- filename = "__Orbital Ion Cannon__/graphics/crosshairsEntity.png",
 				-- priority = "low",
 				-- width = 64,
 				-- height = 64,
@@ -316,7 +316,7 @@ data:extend({
 		show_when_smoke_off = true,
 		animation =
 		{
-			filename = "__Orbital-Ion-Cannon__/graphics/explosion.png",
+			filename = "__Orbital Ion Cannon__/graphics/explosion.png",
 			priority = "low",
 			width = 192,
 			height = 192,
@@ -339,7 +339,7 @@ data:extend({
 		show_when_smoke_off = true,
 		animation =
 		{
-			filename = "__Orbital-Ion-Cannon__/graphics/IonBeam.png",
+			filename = "__Orbital Ion Cannon__/graphics/IonBeam.png",
 			priority = "low",
 			width = 110,
 			height = 1871,
@@ -379,7 +379,7 @@ data:extend({
 		sound =
 		{
 			{
-				filename = "__Orbital-Ion-Cannon__/sound/OrbitalIonCannon.ogg",
+				filename = "__Orbital Ion Cannon__/sound/OrbitalIonCannon.ogg",
 				volume = 2.0
 			},
 		},
